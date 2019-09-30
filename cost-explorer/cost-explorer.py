dc0abccf1bd0157fd07045c09bce5d13d81c542a
ridicule777 <msjadhav88@gmail.com>
2019-09-30 08:35:17 +0100

    Changed the way it gets the start date and the end date

--- cost-explorer/cost-explorer.py
+++ cost-explorer/cost-explorer.py
@@ -27,8 +27,11 @@ def get_number_of_days_in_a_month():
         previous_month = int(month - 1)
         year = year
 
-    total_number_of_days_in_previous_month = monthrange(year, previous_month)
-    return year + '-' + previous_month + '-' + total_number_of_days_in_previous_month.split('-')
+    # total_number_of_days_in_previous_month = monthrange(year, previous_month)
+    # return year + '-' + previous_month + '-' + total_number_of_days_in_previous_month.split('-')
+    total_number_of_days_in_previous_month = monthrange(year, previous_month)[
+        1]
+    return total_number_of_days_in_previous_month
 
 
 def get_project_list():
@@ -62,18 +65,28 @@ def get_cost_based_on_tags(project_list, region, start, end):
 
 if __name__ == "__main__":
 
-    previous_date = get_number_of_days_in_a_month()
-    year = int(previous_date[0])
-    month = int(previous_date[1])
-    day = int(previous_date[2])
-    start = year + '-' + month + '-' + '01'
-    end = year + '-' + month + '-' + day
-    parser = argparse.ArgumentParser()
+    total_number_of_days_in_previous_month = get_number_of_days_in_a_month()
+    # previous_date = get_number_of_days_in_a_month()
+    # year = int(previous_date[0])
+    # month = int(previous_date[1])
+    # day = int(previous_date[2])
+    # start = year + '-' + month + '-' + '01'
+    # end = year + '-' + month + '-' + day
+    end = now.strftime('%Y-%m-%d')
+    get_start_date = now - \
+        datetime.timedelta(days=total_number_of_days_in_previous_month)
+    start_year = str(get_start_date).split(' ')[0].split("-")[0])
+    start_month=str(get_start_date).split(' ')[0].split("-")[1])
+    start_date=str(get_start_date).split(' ')[0].split("-")[2])
+    start="{year}-{month}-{date}".format(year=start_year,
+                                         month=start_month, date=start_date)
+    # start = datetime.timedelta(days=total_number_of_days_in_previous_month)
+    parser=argparse.ArgumentParser()
     parser.add_argument('-d', '--days', type=int,
                         default=total_number_of_days_in_previous_month)
     parser.add_argument('-r', '--region', type=string,
                         default="ap-south-1")
-    args = parser.parse_args()
-    region = args.region
-    project_list = get_project_list()
+    args=parser.parse_args()
+    region=args.region
+    project_list=get_project_list()
     get_cost_based_on_tags(project_list, region, start, end)
